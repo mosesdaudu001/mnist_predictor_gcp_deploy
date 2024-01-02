b35700a179bd1ca7283230d8b43d72b9c8b5b745
Moses Daudu <mosesdaudu001@gmail.com>
2024-01-02 10:44:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,22 +5,20 @@ resp = requests.post("http://127.0.0.1:5000", files={'file': open('./eight.png',
 
 print(resp.json())
 
-
 # import io
 # import tensorflow as tf
 # from tensorflow import keras
 # import numpy as np
 # from PIL import Image
 # from google.cloud import storage
+# import tensorflow.lite as tflite
 
 # from flask import Flask, request, jsonify
 
 # storage_client = storage.Client()
-# bucket = storage_client.get_bucket('mnist-moses-buc')
-# blob_classifier = bucket.blob('models/nn.h5')
-# blob_classifier.download_to_filename('tmp/nn.h5')
-
-# model = keras.models.load_model("tmp/nn.h5")
+# bucket = storage_client.get_bucket('moses-daudu-bucket')
+# blob_classifier = bucket.blob('models/mnist-model.tflite')
+# blob_classifier.download_to_filename('tmp/mnist-model.tflite')
 
 # def transform_image(pillow_image):
 #   data = np.asarray(pillow_image)
@@ -28,6 +26,7 @@ print(resp.json())
 #   data = data[np.newaxis, ..., np.newaxis]
 #   # --> [1, x, y, 1]
 #   data = tf.image.resize(data, [28, 28])
+#   data = np.array(data)
 #   return data
 
 
@@ -38,19 +37,41 @@ print(resp.json())
 #   label0 = np.argmax(pred0)
 #   return label0
 
-
 # def hello_world(request):
-#   file = request.files.get('file')
-#   if file is None or file.filename == "":
+#   img_file = request.files.get('file')
+#   if img_file is None or img_file.filename == "":
 #     return jsonify({"error": "no file"})
 #   try:
-#     image_bytes = file.read()
+#     image_bytes = img_file.read()
 #     pillow_img = Image.open(io.BytesIO(image_bytes)).convert('L')
-#     tensor = transform_image(pillow_img)
-#     prediction = predict(tensor)
+#     X = transform_image(pillow_img)
+
+#     interpreter = tflite.Interpreter(model_path='tmp/mnist-model.tflite')
+#     interpreter.allocate_tensors()
+
+#     # Get the input and output details
+#     input_details = interpreter.get_input_details()
+#     output_details = interpreter.get_output_details()
+
+#     # Ensure the input tensor has the correct shape
+#     interpreter.resize_tensor_input(input_details[0]['index'], X.shape)
+#     interpreter.allocate_tensors()
+
+#     # Set the input tensor
+#     interpreter.set_tensor(input_details[0]['index'], X.astype(np.float32))
+
+#     # Run inference
+#     interpreter.invoke()
+
+#     # Get the output tensor values
+#     preds = interpreter.get_tensor(output_details[0]['index'])
+
+#     # Post-process the output values as needed
+#     prediction = np.argmax(preds, axis=1)
 #     data = {"prediction": int(prediction)}
 #     return jsonify(data)
 #   except Exception as e:
 #     return jsonify({"error": str(e)})
 
 #   return "OK"
+  
